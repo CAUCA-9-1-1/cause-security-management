0221d07bbeee041db8fa1fce5570fd1361f1c191
Jean-Philippe Boucher <jeanphilippe.boucher@cauca.ca>
2019-08-02 08:08:53 -0400

    Adding permissions management

--- Cause.SecurityManagement/Controllers/BasePermissionManagementController.cs
+++ Cause.SecurityManagement/Controllers/BasePermissionManagementController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Cause.SecurityManagement.Models;
 using Cause.SecurityManagement.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -20,5 +21,37 @@ namespace Cause.SecurityManagement.Controllers
 		{
 			return PermissionService.GetPermissions();
 		}
-	}
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(401)]
+        public ActionResult Add([FromBody]ModulePermission permission)
+        {
+            var result = PermissionService.Add(permission);
+            if (result)
+                return Ok();
+            return BadRequest();
+        }
+
+        [HttpPost("Update")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(401)]
+        public ActionResult Update([FromBody]ModulePermission permission)
+        {
+            var result = PermissionService.Update(permission);
+            if (result)
+                return Ok();
+            return BadRequest();
+        }
+
+        [HttpDelete("{permissionId}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(401)]
+        public ActionResult Delete(Guid permissionId)
+        {
+            if (PermissionService.Delete(permissionId))
+                return Ok();
+            return BadRequest();
+        }
+    }
 }
