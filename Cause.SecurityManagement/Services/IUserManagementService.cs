294fdcddf48a642942215bc9bdcadfe8a9a5c929
Claudine <claudine.gagnon@cauca.ca>
2020-02-05 09:59:59 -0500

    #41_CurrentUserShouldNotBeAttachedToContext:
    Implémentaire l'interface IUserManagementService

---
+++ Cause.SecurityManagement/Services/IUserManagementService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Cause.SecurityManagement.Models;
+using Cause.SecurityManagement.Models.DataTransferObjects;
+
+namespace Cause.SecurityManagement.Services
+{
+    public interface IUserManagementService<TUser>
+    {
+        List<TUser> GetActiveUsers();
+        TUser GetUser(Guid userId);
+        bool UpdateUser(TUser user, string applicationName);
+        bool UserNameAlreadyUsed(TUser user);
+        bool ChangePassword(Guid userId, string newPassword, string applicationName);
+        bool DeactivateUser(Guid userId);
+        List<UserGroup> GetGroups(Guid userId);
+        bool AddGroup(UserGroup group);
+        bool RemoveGroup(Guid userGroupId);
+        bool UpdatePermission(UserPermission permission);
+        bool RemovePermission(Guid userPermissionId);
+        List<UserMergedPermission> GetPermissionsForUser(Guid userId);
+    }
+}
