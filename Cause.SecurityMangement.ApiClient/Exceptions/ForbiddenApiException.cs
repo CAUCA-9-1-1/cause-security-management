63529b464d11524c46bb4a883269104daafb072f
Michael <michael.jolin@cauca.ca>
2021-10-21 10:13:34 -0400

    fix() Return ApiClientException on 403

--- Cause.SecurityMangement.ApiClient/Exceptions/ForbiddenApiException.cs
+++ Cause.SecurityMangement.ApiClient/Exceptions/ForbiddenApiException.cs
@@ -1,14 +1,10 @@
-﻿using Flurl.Http;
+﻿using System;
 
 namespace Cauca.ApiClient.Exceptions
 {
 	public class ForbiddenApiException : ApiClientException
 	{
-		public ForbiddenApiException(string url) : base($"API returned a 403 (forbidden) response for url '{url}'.")
-		{
-		}
-
-		public ForbiddenApiException(string url, FlurlHttpException innerException) : base($"API returned a 403 (forbidden) response for url '{url}'.", innerException)
+		public ForbiddenApiException(string url, Exception innerException = null) : base($"API returned a 403 (forbidden) response for url '{url}'.", innerException)
 		{
 		}
 	}
