f345b5acaa3e221f5998db38f76cec0957cc9b5f
Michael <michael.jolin@cauca.ca>
2022-06-02 11:48:09 -0400

    Feat(MA-1830) Base class for all ValidationCodeResult

--- Cause.SecurityManagement.Models/ValidationCode/EmailValidationCodeResult.cs
+++ Cause.SecurityManagement.Models/ValidationCode/EmailValidationCodeResult.cs
@@ -1,12 +1,7 @@
-﻿using System;
-
-namespace Cause.SecurityManagement.Models.ValidationCode
+﻿namespace Cause.SecurityManagement.Models.ValidationCode
 {
-	public class EmailValidationCodeResult
+	public class EmailValidationCodeResult : BaseValidationCodeResult
 	{
-		public Guid UserId { get; set; }
-		public string UserName { get; set; }
 		public string Email { get; set; }
-        public ValidationCodeType Type { get; set; }
 	}
 }
