41fd29663e3ae1b92572900d907c8f6445b08284
Eric Fortin <eric.fortin@cauca.ca>
2022-08-17 16:11:53 -0400

    MA-1792 Ajout du allGroupAccess permission

--- Cause.SecurityManagement.Models/Group.cs
+++ Cause.SecurityManagement.Models/Group.cs
@@ -5,8 +5,9 @@ namespace Cause.SecurityManagement.Models
 	public class Group : BaseModel
 	{
 		public string Name { get; set; }
+        public bool AssignableByAllUsers { get; set; }
 
-		public ICollection<GroupPermission> Permissions { get; set; }
+        public ICollection<GroupPermission> Permissions { get; set; }
         public ICollection<UserGroup> Users { get; set; }
 	}
 }
