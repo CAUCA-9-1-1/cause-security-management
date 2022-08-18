12fbb02f668e082291ae80a1792318f9c738c920
Eric Fortin <eric.fortin@cauca.ca>
2022-08-18 08:55:25 -0400

    MA-1792 Extraction context en repository

---
+++ Cause.SecurityManagement/Repositories/IGroupPermissionRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Cause.SecurityManagement.Models;
+
+namespace Cause.SecurityManagement.Repositories
+{
+    public interface IGroupPermissionRepository
+    {
+        IQueryable<GroupPermission> GetForGroup(Guid groupId);
+        GroupPermission Get(Guid groupPermissinId);
+        bool Any(Guid groupPermissionId);
+        void Add(GroupPermission groupPermission);
+        void Remove(GroupPermission groupPermission);
+        void Update(GroupPermission groupPermission);
+        void SaveChanges();
+    }
+}
---
+++ Cause.SecurityManagement/Repositories/IUserGroupRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Cause.SecurityManagement.Models;
+
+namespace Cause.SecurityManagement.Repositories
+{
+    public interface IUserGroupRepository
+    {
+        IQueryable<UserGroup> GetForGroup(Guid groupId);
+        UserGroup Get(Guid userGroupId);
+        bool Any(Guid userGroupId);
+        void Add(UserGroup userGroup);
+        void Remove(UserGroup userGroup);
+        void Update(UserGroup userGroup);
+        void SaveChanges();
+    }
+}
