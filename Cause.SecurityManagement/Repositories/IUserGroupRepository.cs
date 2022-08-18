85b9c9af23d91a7f91c3bd6ecc86c101a12aa71c
Eric Fortin <eric.fortin@cauca.ca>
2022-08-18 11:01:13 -0400

    MA-1792 Enleve userGroupContext de BAseUserManagementService

--- Cause.SecurityManagement/Repositories/IUserGroupRepository.cs
+++ Cause.SecurityManagement/Repositories/IUserGroupRepository.cs
@@ -7,6 +7,7 @@ namespace Cause.SecurityManagement.Repositories
     public interface IUserGroupRepository
     {
         IQueryable<UserGroup> GetForGroup(Guid groupId);
+        IQueryable<UserGroup> GetForUser(Guid userId);
         UserGroup Get(Guid userGroupId);
         bool Any(Guid userGroupId);
         void Add(UserGroup userGroup);
